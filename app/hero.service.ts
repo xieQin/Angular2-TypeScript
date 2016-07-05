1171109c80abd0e02852b4a1cc1514eb2e794075
cherish <cherishhsieh@gmail.com>
2016-07-05 08:17:37 +0800

    modify service

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,6 +6,9 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
   getHeroes() {
-    return Promise.resolve(HEROES);
+    // return Promise.resolve(HEROES);
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+    );
   }
 }
