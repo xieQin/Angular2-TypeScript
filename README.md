95c710444f7cf1a3d1050ea66cfd396aaa723712
cherish <cherishhsieh@gmail.com>
2016-07-06 22:12:08 +0800

    add routes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Angular2-TypeScript
 
+[quickstart](https://angular.io/docs/ts/latest/quickstart.html)
+
 ```bash
 npm install
 npm run typings install
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,19 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 @Component({
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
 })
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit, OnDestroy {
   hero: Hero;
+  sub: any;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
